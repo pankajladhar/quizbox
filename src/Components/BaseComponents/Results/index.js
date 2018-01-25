b2768e15a0acde226fedad6f9ec451fde09d2654
pankajladhar <ladharpankaj@gmail.com>
2018-01-25 14:00:28 +0530

    Added worng answer list

--- src/Components/BaseComponents/Results/index.js
+++ src/Components/BaseComponents/Results/index.js
@@ -8,12 +8,16 @@ import './Results.css'
 class Results extends PureComponent {
     constructor(props) {
         super(props);
+        this.state = {
+            showAnswers: false
+        }
         this.getResults = this.getResults.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     getResults() {
         let correctAnswerCount = 0
-        let resultListArray = []
+        let resultListArray = [];
         this.props.userAnswers.forEach(element => {
             if (element.correctAnswer === element.answer) {
                 correctAnswerCount = correctAnswerCount + 1;
@@ -22,33 +26,45 @@ class Results extends PureComponent {
         return correctAnswerCount;
     }
 
-    getWrongAnswer() {
-        return this.props.userAnswers.filter((item) => {
+    getWrongAnswer(items) {
+        return items.filter((item) => {
             return item.answer !== item.correctAnswer
+        })
+    }
+
+    handleClick() {
+        this.setState({
+            showAnswers: !this.state.showAnswers
         })
     }
 
     render() {
         return (
             <div className="Results">
-                <h1>
-                    Results
-                </h1>
-                {this.getResults()} out of {this.props.noOfques} are correct answer
-                {(this.getResults() / this.props.noOfques) * 100} %
-                <Button className="btn"
-                    value="Get Correct Answer" />
-                <div className="ResultList__Wrapper">
-                    {
-                        this.getWrongAnswer().map((item) => {
-                            return (
-                                <ResultList question={item.question}
-                                    userAnswer={item.answer}
-                                    correctAnswer={item.correctAnswer} />
-                            )
-                        })
-                    }
+                <div className="Results__TopPanel">
+                    <span className="Results__TopPanel__CorrectCount">
+                        {this.getResults()} out of {this.props.noOfques} are correct answer
+                    </span>
+                    <span className="Results__TopPanel__Percentage">
+                        {(this.getResults() / this.props.noOfques) * 100} %
+                    </span>
+                    <Button className="btn"
+                        onClick={this.handleClick}
+                        value="Get Correct Answer" />
                 </div>
+                {
+                    this.state.showAnswers && <div className="ResultList__Wrapper">
+                        {
+                            this.getWrongAnswer(this.props.userAnswers).map((item) => {
+                                return (
+                                    <ResultList question={item.question}
+                                        userAnswer={item.answer}
+                                        correctAnswer={item.correctAnswer} />
+                                )
+                            })
+                        }
+                    </div>
+                }
             </div>
         );
     }
