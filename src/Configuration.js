1f83c5d1b892b07156269f8ea3d43c2589aefbe8
pankajladhar <ladharpankaj@gmail.com>
2018-02-13 15:00:54 +0530

    Fixing some styling

--- src/Configuration.js
+++ src/Configuration.js
@@ -1,13 +1,12 @@
 const Configuration = {
     "FireBaseConfiguration": {
-        "apiKey": "API_KEY",
-        "authDomain": "authDomain",
-        "databaseURL": "databaseURL",
-        "projectId": "projectId",
-        "storageBucket": "storageBucket",
-        "messagingSenderId": "messagingSenderId"
+        "apiKey": "AIzaSyCAh_AzERVuES3pV9dbIpVnsKEt8KHo5p8",
+        "authDomain": "onlinequizcreator-d252a.firebaseapp.com",
+        "databaseURL": "https://onlinequizcreator-d252a.firebaseio.com",
+        "projectId": "onlinequizcreator-d252a",
+        "storageBucket": "",
+        "messagingSenderId": "744885560956"
     }
 }
 
 export default Configuration;
-
