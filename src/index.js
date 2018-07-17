6f1fb5bce89ad5f7a92f370d04d654fa988cbca0
pankajladhar <ladharpankaj@gmail.com>
2018-07-17 19:45:36 +0530

    Some clean up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './Components/BusinessComponents/App';
-// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
