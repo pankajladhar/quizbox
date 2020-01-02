ce58d31f9cf8650aebefe00ff87f979ea9b2f62e
pankajladhar <ladharpankaj@gmail.com>
2020-01-02 22:05:38 +0530

    Covert Answer component into functional component

--- src/Components/BaseComponents/Answer/index.js
+++ src/Components/BaseComponents/Answer/index.js
@@ -1,29 +1,24 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import RadioButton from './../RadioButton';
-import './Answer.scss';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import RadioButton from "./../RadioButton";
+import "./Answer.scss";
 
-class Answer extends Component {
-    render() {
-        return (
-            <div className={`Answer ${this.props.checked ? 'checked' : 'unchecked'}`}>
-                <span>{this.props.index + 1}.</span>
-                <RadioButton
-                    checked={this.props.checked}
-                    id={this.props.id}
-                    onChange={this.props.onChange}
-                    text={this.props.data} />
-            </div>
-        );
-    }
-}
+const Answer = props => {
+  const { checked, index, id, onChange, data } = props;
+  return (
+    <div className={`Answer ${checked ? "checked" : "unchecked"}`}>
+      <span>{index + 1}.</span>
+      <RadioButton checked={checked} id={id} onChange={onChange} text={data} />
+    </div>
+  );
+};
 
 Answer.propTypes = {
-    index: PropTypes.number.isRequired,
-    checked: PropTypes.bool.isRequired,
-    id: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-    data: PropTypes.string.isRequired
+  index: PropTypes.number.isRequired,
+  checked: PropTypes.bool.isRequired,
+  id: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  data: PropTypes.string.isRequired
 };
 
 export default Answer;
