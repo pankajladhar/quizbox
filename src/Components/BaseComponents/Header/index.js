3af7babf80eff9fd55eeb204c8e2e87099c08210
pankajladhar <ladharpankaj@gmail.com>
2020-01-02 22:04:03 +0530

    Covert header component into functional component

--- src/Components/BaseComponents/Header/index.js
+++ src/Components/BaseComponents/Header/index.js
@@ -1,30 +1,29 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import Navigation from './../Navigation';
-import './Header.scss'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import Navigation from "./../Navigation";
+import "./Header.scss";
 
-class Header extends Component {
-    render() {
-        return (
-            <div className="Header">
-                <header className="container">
-                    <div className="Header__Branding">
-                        <Link to="/">
-                            <img className="Header__Logo" src={this.props.logoUrl} alt={this.props.title} />
-                            {this.props.title && <h1 className="Header__title">{this.props.title}</h1>}
-                        </Link>
-                    </div>
-                    <Navigation />
-                </header>
-            </div>
-        );
-    }
-}
+const Header = props => {
+  const { title, logoUrl } = props;
+  return (
+    <div className="Header">
+      <header className="container">
+        <div className="Header__Branding">
+          <Link to="/">
+            <img className="Header__Logo" src={logoUrl} alt={title} />
+            {title && <h1 className="Header__title">{title}</h1>}
+          </Link>
+        </div>
+        <Navigation />
+      </header>
+    </div>
+  );
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-    logoUrl: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  logoUrl: PropTypes.string.isRequired
 };
 
 export default Header;
